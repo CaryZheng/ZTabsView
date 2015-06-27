679d91e86862d8dc7b1cdc1ff8918d508bb5631d
CaryZheng <CaryZhengMy@gmail.com>
2015-06-27 21:30:01 +0800

    Call ZTabsView

--- ZTabsViewDemo/ZTabsViewDemo/ViewController.swift
+++ ZTabsViewDemo/ZTabsViewDemo/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, IZTabsView {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initTabsView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func initTabsView()
+    {
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        let parentWidth = self.view.bounds.size.width
+        let parentHeight = self.view.bounds.size.height
+        
+        var tabsView = ZTabsView()
+        tabsView.delegate = self
+        
+        tabsView.frame = CGRectMake(0, statusBarHeight, parentWidth, parentHeight)
+        
+        let tabTitles = ["Tab1", "Tab2", "Tab3"]
+        
+        var view1 = UIView()
+        view1.backgroundColor = UIColor.redColor()
+        
+        var view2 = UIView()
+        view2.backgroundColor = UIColor.yellowColor()
+        
+        var view3 = UIView()
+        view3.backgroundColor = UIColor.blueColor()
+        
+        let contentViews = [view1, view2, view3]
+        tabsView.initView(tabTitles, contentViews: contentViews)
+        
+        self.view.addSubview(tabsView)
+    }
+    
+    func onTabChanged(currentTabIndex: Int, lastTabIndex: Int)
+    {
+        ZLog.d("onTabChanged currentTabIndex = \(currentTabIndex), lastTabIndex = \(lastTabIndex)")
+    }
 }
 
